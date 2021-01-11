3f33f26bd9b32b4c58994aa32b42a078ea370652
au614889 <bl@eng.au.dk>
2021-01-11 11:03:17 +0100

    Docs

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="",
-    version="0.0.1",
-    author="",
-    author_email="",
-    description="",
+    name="UR-data",
+    version="0.1",
+    author="Błażej Leporowski",
+    author_email="bleporowski@outlook.com",
+    description="Accompanying library to UR screwdriver dataset",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
