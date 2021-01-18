b3533408cf3d153c7139ba4b43ab1d23d62c3c73
au614889 <bl@eng.au.dk>
2021-01-18 11:05:18 +0100

    Release 0.1

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 A python library for the UR Screwdriver dataset as described in [link].
 
 The UR-data library contains several useful functionalities for preprocessing the dataset for ML applications:
-* Creating numpy training and test datasets from the original file
+* Creating numpy training and test datasets for sampled data
+* Creating a [Keras TimeSeries generators](https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/sequence/TimeseriesGenerator) 
+  for sliding window data
 * Filtering the dataset
 * Removing undesired columns as outlined in the paper
 * 2 different types of labeling
@@ -39,20 +41,21 @@ pip install urdata
 To install latest version from github, clone the source from the project repository and install with setup.py:
 ```bash
 git clone https://github.com/CptPirx/robo-package
-cd UR-data
+cd robo-package
 python setup.py install --user
 ```
 ## Instructions
 
-The package presents to user a single method get_dataset_numpy(). Its parameters are:
+The package presents to user two methods: get_dataset_numpy() and get_dataset_generator().
+
+#### Sampling
 ```bash
-def get_dataset(path, onehot_labels=True, sliding_window=False, window_size=100,
-                reduce_dimensionality=False, reduce_method='PCA', n_dimensions=60, subsample_data=True,
-                subsample_freq=5, pad_data=True, train_size=0.7, random_state=42, normal_samples=1,
-                damaged_samples=1, assembly_samples=1, missing_samples=1, damaged_thread_samples=0,
-                loosening_samples=1, drop_loosen=True, drop_extra_columns=True, label_full=False, batch_size=256):
+def get_dataset_numpy(path, onehot_labels=True, reduce_dimensionality=False, reduce_method='PCA', n_dimensions=60,
+                      subsample_data=True, subsample_freq=2, train_size=0.7, random_state=42, normal_samples=1,
+                      damaged_samples=1, assembly_samples=1, missing_samples=1, damaged_thread_samples=0,
+                      loosening_samples=1, drop_loosen=True, drop_extra_columns=True, label_full=False):
     """
-    Create a numpy dataset from input dataframe
+    Create numpy dataset from input h5 file
 
     :param path: path to the data
     :param label_full: bool, 
@@ -65,7 +68,7 @@ def get_dataset(path, onehot_labels=True, sliding_window=False, window_size=100,
         percentage of missing samples to take
     :param assembly_samples: float, 
         percentage of extra assembly samples to take
-    :param damaged_samples: float, 
+    :param damaged_samples: float,
         percentage of damaged samples to take
     :param normal_samples: float, 
         percentage of normal samples to take
@@ -77,9 +80,7 @@ def get_dataset(path, onehot_labels=True, sliding_window=False, window_size=100,
         random state for train_test split
     :param train_size: float, 
         percentage of data as training data
-    :param pad_data: bool, 
-        pad data to create even size set of samples
-    :param subsample_freq: int, 
+    :param subsample_freq: int,
         the frequency of subsampling
     :param subsample_data: bool, 
         reduce number of events by taking every subsample_freq event
@@ -89,17 +90,84 @@ def get_dataset(path, onehot_labels=True, sliding_window=False, window_size=100,
         dimensionality reduction method to be used
     :param n_dimensions: int, 
         the target number of dimensions
-    :param sliding_window: bool, 
-        create a sliding window dataset
-    :param window_size: int, 
-        size of the sliding window
     :param onehot_labels: bool, 
         output onehot encoded labels
-    :param batch_size: int, 
-        batch size for the sliding window generator
 
-    :return: np arrays, train and test data & labels
+    :return: 4 np arrays, train and test data & labels
     """
 ```
 
-This method will return 4 numpy arrays: train_x, train_y, test_x and test_y. 
+Sample usage:
+```bash
+import urdata.urdata
+
+data_path = 'C:/Users/my_path/robot_data.h5'
+
+train_x, train_y, test_x, test_y = urdata.get_dataset_numpy(data_path)
+```
+
+#### Sliding window
+
+
+```bash
+def get_dataset_generator(path, window_size=100, reduce_dimensionality=False, reduce_method='PCA', n_dimensions=60,
+                          subsample_data=True, subsample_freq=2, train_size=0.7, random_state=42, normal_samples=1,
+                          damaged_samples=1, assembly_samples=1, missing_samples=1, damaged_thread_samples=0,
+                          loosening_samples=1, drop_loosen=True, drop_extra_columns=True, label_full=False,
+                          batch_size=256):
+    """
+    Create Keras sliding window generator from input h5 file
+
+    :param path: path to the data
+    :param label_full: bool,
+        both loosening and tightening are part of one label
+    :param drop_extra_columns: bool,
+        drop the extra columns as outlined in the paper
+    :param drop_loosen: bool,
+        drop the loosening columns
+    :param missing_samples: float,
+        percentage of missing samples to take
+    :param assembly_samples: float,
+        percentage of extra assembly samples to take
+    :param damaged_samples: float,
+        percentage of damaged samples to take
+    :param normal_samples: float,
+        percentage of normal samples to take
+    :param loosening_samples: float,
+        percentage of loosening samples to take
+    :param damaged_thread_samples: float,
+        percentage of damaged thread samples to take
+    :param random_state: int,
+        random state for train_test split
+    :param train_size: float,
+        percentage of data as training data
+    :param subsample_freq: int,
+        the frequency of subsampling
+    :param subsample_data: bool,
+        reduce number of events by taking every subsample_freq event
+    :param reduce_dimensionality: bool,
+        reduce dimensionality of the dataset
+    :param reduce_method: string,
+        dimensionality reduction method to be used
+    :param n_dimensions: int,
+        the target number of dimensions
+    :param window_size: int,
+        size of the sliding window
+    :param batch_size: int,
+        batch size for the sliding window generator
+
+    :return: 2 np arrays,
+        full continous data and labels
+    :return: 2 keras TimeSeries generators,
+        train and test generators
+    """
+```
+
+Sample usage:
+```bash
+import urdata.urdata
+
+data_path = 'C:/Users/my_path/robot_data.h5'
+
+data, labels, train_generator, test_generator = urdata.get_dataset_generator(data_path)
+```
--- urdata/urdata.py
+++ urdata/urdata.py
@@ -2,7 +2,6 @@ __doc__ = "Data loader for the UR robot dataset"
 __author__ = "Błażej Leporowski"
 __version__ = "Version 0.1 # 11/01/2021 # Initial release #"
 
-import sys
 import numpy as np
 
 from sklearn.model_selection import train_test_split
@@ -12,36 +11,48 @@ from .utils import load_dataset, reduce_dimensions, subsample, pad_df, pd_to_np,
     print_info, create_window_generator
 
 
-def get_dataset(path, onehot_labels=True, sliding_window=False, window_size=100,
-                reduce_dimensionality=False, reduce_method='PCA', n_dimensions=60, subsample_data=True,
-                subsample_freq=5, pad_data=True, train_size=0.7, random_state=42, normal_samples=1,
-                damaged_samples=1, assembly_samples=1, missing_samples=1, damaged_thread_samples=0,
-                loosening_samples=1, drop_loosen=True, drop_extra_columns=True, label_full=False, batch_size=256):
+def get_dataset_numpy(path, onehot_labels=True, reduce_dimensionality=False, reduce_method='PCA', n_dimensions=60,
+                      subsample_data=True, subsample_freq=2, train_size=0.7, random_state=42, normal_samples=1,
+                      damaged_samples=1, assembly_samples=1, missing_samples=1, damaged_thread_samples=0,
+                      loosening_samples=1, drop_loosen=True, drop_extra_columns=True, label_full=False):
     """
-    Create a numpy dataset from input dataframe
+    Create numpy dataset from input h5 file
 
     :param path: path to the data
-    :param label_full: bool, both loosening and tightening are part of one label
-    :param drop_extra_columns: bool, drop the extra columns as outlined in the paper
-    :param drop_loosen: bool, drop the loosening columns
-    :param missing_samples: float, percentage of missing samples to take
-    :param assembly_samples: float, percentage of extra assembly samples to take
-    :param damaged_samples: float, percentage of damaged samples to take
-    :param normal_samples: float, percentage of normal samples to take
-    :param loosening_samples: float, percentage of loosening samples to take
-    :param damaged_thread_samples: float, percentage of damaged thread samples to take
-    :param random_state: int, random state for train_test split
-    :param train_size: float, percentage of data as training data
-    :param pad_data: bool, pad data to create even size set of samples
-    :param subsample_freq: int, the frequency of subsampling
-    :param subsample_data: bool, reduce number of events by taking every subsample_freq event
-    :param reduce_dimensionality: bool, reduce dimensionality of the dataset
-    :param reduce_method: string, dimensionality reduction method to be used
-    :param n_dimensions: int, the target number of dimensions
-    :param sliding_window: bool, create a sliding window dataset
-    :param window_size: int, size of the sliding window
-    :param onehot_labels: bool, output onehot encoded labels
-    :param batch_size: int, batch size for the sliding window generator
+    :param label_full: bool,
+        both loosening and tightening are part of one label
+    :param drop_extra_columns: bool,
+        drop the extra columns as outlined in the paper
+    :param drop_loosen: bool,
+        drop the loosening columns
+    :param missing_samples: float,
+        percentage of missing samples to take
+    :param assembly_samples: float,
+        percentage of extra assembly samples to take
+    :param damaged_samples: float,
+        percentage of damaged samples to take
+    :param normal_samples: float,
+        percentage of normal samples to take
+    :param loosening_samples: float,
+        percentage of loosening samples to take
+    :param damaged_thread_samples: float,
+        percentage of damaged thread samples to take
+    :param random_state: int,
+        random state for train_test split
+    :param train_size: float,
+        percentage of data as training data
+    :param subsample_freq: int,
+        the frequency of subsampling
+    :param subsample_data: bool,
+        reduce number of events by taking every subsample_freq event
+    :param reduce_dimensionality: bool,
+        reduce dimensionality of the dataset
+    :param reduce_method: string,
+        dimensionality reduction method to be used
+    :param n_dimensions: int,
+        the target number of dimensions
+    :param onehot_labels: bool,
+        output onehot encoded labels
 
     :return: np arrays, train and test data & labels
     """
@@ -73,18 +84,7 @@ def get_dataset(path, onehot_labels=True, sliding_window=False, window_size=100,
     print('Padding data')
     data = pad_df(data)
 
-    if pad_data:
-        squeeze = True
-    else:
-        squeeze = False
-
-    data, labels = pd_to_np(data, squeeze)
-
-    if not pad_data:
-        data = data.reshape(-1, n_dimensions)
-        added_rows = np.where(np.all(data == 0, axis=1))
-        data = data[~added_rows[0]]
-        labels = labels[~added_rows[0]]
+    data, labels = pd_to_np(data, squeeze=True)
 
     # Split the data
     train_x, test_x, train_y, test_y = train_test_split(data,
@@ -93,22 +93,112 @@ def get_dataset(path, onehot_labels=True, sliding_window=False, window_size=100,
                                                         random_state=random_state,
                                                         stratify=labels)
 
-    if not sliding_window:
-        if onehot_labels:
-            encoder = OneHotEncoder()
-            train_y = encoder.fit_transform(X=train_y.reshape(-1, 1)).toarray()
-            test_y = encoder.fit_transform(X=test_y.reshape(-1, 1)).toarray()
+    if onehot_labels:
+        encoder = OneHotEncoder()
+        train_y = encoder.fit_transform(X=train_y.reshape(-1, 1)).toarray()
+        test_y = encoder.fit_transform(X=test_y.reshape(-1, 1)).toarray()
 
-    else:
-        train_generator, test_generator = create_window_generator(train_x=train_x,
-                                                                  train_y=train_y,
-                                                                  test_x=test_x,
-                                                                  test_y=test_y,
-                                                                  window=window_size,
-                                                                  batch_size=batch_size)
+    return train_x, train_y, test_x, test_y
 
-    if not sliding_window:
-        return train_x, train_y, test_x, test_y
-    else:
-        return data, labels, train_generator, test_generator
 
+def get_dataset_generator(path, window_size=100, reduce_dimensionality=False, reduce_method='PCA', n_dimensions=60,
+                          subsample_data=True, subsample_freq=2, train_size=0.7, random_state=42, normal_samples=1,
+                          damaged_samples=1, assembly_samples=1, missing_samples=1, damaged_thread_samples=0,
+                          loosening_samples=1, drop_loosen=True, drop_extra_columns=True, label_full=False,
+                          batch_size=256):
+    """
+    Create Keras sliding window generator from input h5 file
+
+    :param path: path to the data
+    :param label_full: bool,
+        both loosening and tightening are part of one label
+    :param drop_extra_columns: bool,
+        drop the extra columns as outlined in the paper
+    :param drop_loosen: bool,
+        drop the loosening columns
+    :param missing_samples: float,
+        percentage of missing samples to take
+    :param assembly_samples: float,
+        percentage of extra assembly samples to take
+    :param damaged_samples: float,
+        percentage of damaged samples to take
+    :param normal_samples: float,
+        percentage of normal samples to take
+    :param loosening_samples: float,
+        percentage of loosening samples to take
+    :param damaged_thread_samples: float,
+        percentage of damaged thread samples to take
+    :param random_state: int,
+        random state for train_test split
+    :param train_size: float,
+        percentage of data as training data
+    :param subsample_freq: int,
+        the frequency of subsampling
+    :param subsample_data: bool,
+        reduce number of events by taking every subsample_freq event
+    :param reduce_dimensionality: bool,
+        reduce dimensionality of the dataset
+    :param reduce_method: string,
+        dimensionality reduction method to be used
+    :param n_dimensions: int,
+        the target number of dimensions
+    :param window_size: int,
+        size of the sliding window
+    :param batch_size: int,
+        batch size for the sliding window generator
+
+    :return: np arrays,
+        full continous data and labels
+    :return: keras TimeSeries generators,
+        train and test generators
+    """
+    data = load_dataset(path=path)
+
+    print('Loaded data')
+
+    if label_full:
+        drop_loosen = False
+        data = relabel(data)
+
+    data = drop_columns(data, drop_extra_columns=drop_extra_columns, drop_loosen=drop_loosen)
+
+    if subsample_data:
+        print('Subsampling data')
+        data = subsample(data, subsample_freq)
+
+    if normal_samples < 1 or damaged_samples < 1 or assembly_samples < 1 or missing_samples < 1 or damaged_thread_samples < 1 or loosening_samples < 1:
+        print('Filtering samples')
+        data = filter_samples(data, normal_samples, damaged_samples, assembly_samples, missing_samples,
+                              damaged_thread_samples, loosening_samples)
+
+    print_info(data)
+
+    if reduce_dimensionality:
+        print('Reducing dimensionality')
+        data = reduce_dimensions(data, method=reduce_method, dimensions=n_dimensions)
+
+    print('Padding data')
+    data = pad_df(data)
+
+    data, labels = pd_to_np(data, squeeze=False)
+
+    data = data.reshape(-1, n_dimensions)
+    added_rows = np.where(np.all(data == 0, axis=1))
+    data = data[~added_rows[0]]
+    labels = labels[~added_rows[0]]
+
+    # Split the data
+    train_x, test_x, train_y, test_y = train_test_split(data,
+                                                        labels,
+                                                        train_size=train_size,
+                                                        random_state=random_state,
+                                                        stratify=labels)
+
+    train_generator, test_generator = create_window_generator(train_x=train_x,
+                                                              train_y=train_y,
+                                                              test_x=test_x,
+                                                              test_y=test_y,
+                                                              window=window_size,
+                                                              batch_size=batch_size)
+
+    return data, labels, train_generator, test_generator
